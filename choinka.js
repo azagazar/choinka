5ce48dabac90f3b00a50cf9de948a2ecc8e16c49
AgataZarod <a.zarod@alboalbo.com>
2017-04-02 20:56:59 +0200

    dodanie funkcji rysujChoinke

--- choinka.js
+++ choinka.js
@@ -1,19 +1,22 @@
 var stars;
-var poziomy = 5;
+var poziomy;
 
-for (var k = 1; k <= poziomy ; k++) {
-  stars = '';
+function rysujChoinke(poziomy) {
+	for (var k = 1; k <= poziomy ; k++) {
+		stars = '';
 
-  for(var l = poziomy-k; l >= 0 ; l--) {
-    stars += ' '; 
-  }
+		for(var l = poziomy-k; l >= 0 ; l--) {
+			stars += ' '; 
+		}
 
-  for(var l = 1; l <= (k*2)-1 ; l++) {
-    stars += '*'; 
-  }
+		for(var l = 1; l <= (k*2)-1 ; l++) {
+			stars += '*'; 
+		}
 
-  console.log(stars);
-  
-  stars = stars + '**';
-  
+		console.log(stars);
+	}
+
 }
+
+
+rysujChoinke(10)
